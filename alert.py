fc532d9ea4438eb1d88c77bbc6ed72f7ef3fb45a
MunasheLC <m.chifungo4@gmail.com>
2026-03-18 17:36:18 +0000

    ignore test

--- alert.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_user_data(user_id):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    # UNSAFE: String formatting allows SQL Injection
-    query = f"SELECT * FROM users WHERE id = {user_id};"
-    cursor.execute(query)
-    return cursor.fetchone()
