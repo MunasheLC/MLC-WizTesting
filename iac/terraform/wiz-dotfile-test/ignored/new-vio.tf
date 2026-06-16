47be71acac67b10bdc891bcc9aeac64750432379
MunasheLC <m.chifungo4@gmail.com>
2026-06-16 17:21:34 +0100

    ignore rule test 2

--- iac/terraform/wiz-dotfile-test/ignored/new-vio.tf
+++ iac/terraform/wiz-dotfile-test/ignored/new-vio.tf
@@ -1,11 +1,5 @@
-resource "aws_s3_bucket" "sukidarake_dotfile_ignored_existing_dir" {
-  bucket = "sukidarake-dotfile-ignored-existing-dir"
-}
-
-resource "aws_s3_bucket_public_access_block" "ignored_leak" {
-  bucket                  = aws_s3_bucket.sukidarake_dotfile_ignored_existing_dir.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+# Replicating the exact finding from image_eaaa20.png
+resource "aws_ecr_repository" "dotfile_ignored_existing_dir" {
+  name                 = "sukidarake-dotfile-ignored-existing-dir"
+  image_tag_mutability = "MUTABLE" # This triggers CRS-001
 }
