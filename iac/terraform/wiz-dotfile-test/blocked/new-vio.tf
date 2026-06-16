be62f195c79c6e1acacfff6d9dfebb48470a0968
MunasheLC <m.chifungo4@gmail.com>
2026-06-16 17:28:25 +0100

    corrected findings

--- iac/terraform/wiz-dotfile-test/blocked/new-vio.tf
+++ iac/terraform/wiz-dotfile-test/blocked/new-vio.tf
@@ -1,5 +1,5 @@
-# Replicating the exact finding from image_eaaa20.png
-resource "aws_ecr_repository" "dotfile_ignored_existing_dir" {
-  name                 = "sukidarake-dotfile-ignored-existing-dir"
-  image_tag_mutability = "MUTABLE" # This triggers CRS-001
-}
+# # Replicating the exact finding from image_eaaa20.png
+# resource "aws_ecr_repository" "dotfile_ignored_existing_dir" {
+#   name                 = "sukidarake-dotfile-ignored-existing-dir"
+#   image_tag_mutability = "MUTABLE" # This triggers CRS-001
+# }
